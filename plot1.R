eacf543e81b29df8ef80ae0d4768d443d962d456
BaazouziWael <63655723+BaazouziWael@users.noreply.github.com>
2020-05-09 23:51:45 +0100

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -1,3 +1,10 @@
 par(mfrow=c(1,1),mar=c(5,4.5,4,2))
 hist(hpc$Global_active_power, col="red", main="Global Active Power", 
      xlab="Global Active Power (kilowatts)")
+
+
+png("plot1.png", width=480, height=480)
+par(mfrow=c(1,1),mar=c(5,4.5,4,2))
+hist(hpc$Global_active_power, col="red", main="Global Active Power", 
+     xlab="Global Active Power (kilowatts)")
+dev.off()
